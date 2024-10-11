208926a243238b9fa4bee6c2d5df7a34edd8ee75
nishaaa25 <sahnisha2505@gmail.com>
2024-10-11 20:17:06 +0530

    dummy

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -17,7 +17,7 @@ export default function ProjectCard({ imgUrl, title }) {
           href="#"
           className="px-4 py-2 bg-white text-black rounded-3xl text-sm tracking-[1px]"
         >
-          BRANDING
+          BRANDING -hii
         </Link>
         <Link
           href="#"
